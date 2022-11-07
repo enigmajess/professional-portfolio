69f0a1a0759e93e19a1d40ea7e7b4d7ecee3c209
enigmajess <jessica99362@gmail.com>
2022-11-07 12:38:16 -0800

    added simple placeholder text and styling to blog

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,7 +1,10 @@
+import "./Blog.css";
+
 const Blog = () => {
     return (
         <div className="blog">
-            Hello from blog
+           <h1> Under Construction</h1>
+           <h1>🚧</h1>
       </div>
     )
 }
